4cb00314010031475ce957f42c5a32b41a9c349a
spirit-tv <tlitaudon@gmail.com>
2019-12-18 12:00:42 +0100

    login view

--- register.php
+++ register.php
@@ -5,84 +5,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="tailwind.css">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="register.css">
+    <title>Ventura - register</title>
 </head>
 <body>
-<body class="font-mono bg-gray-400">
-		<!-- Container -->
-		<div class="container mx-auto">
-			<div class="flex justify-center px-6 my-12">
-				<!-- Row -->
-				<div class="w-full xl:w-3/4 lg:w-11/12 flex">
-					<!-- Col -->
-					<div
-						class="w-full h-auto bg-gray-400 hidden lg:block lg:w-1/2 bg-cover rounded-l-lg"
-						style="background-image: url('https://www.ventura-roleplay.fr/bundles/images/banner1.jpg')"
-					></div>
-					<!-- Col -->
-					<div class="w-full lg:w-1/2 bg-white p-5 rounded-lg lg:rounded-l-none">
-						<h3 class="pt-4 text-2xl text-center">Inscription!</h3>
-						<form class="px-8 pt-6 pb-8 mb-4 bg-white rounded">
-							<div class="mb-4">
-								<label class="block mb-2 text-sm font-bold text-gray-700" for="username">
-									Username
-								</label>
-								<input
-									class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-									id="username"
-									type="text"
-									placeholder="Username"
-								/>
-							</div>
-							<div class="mb-4">
-								<label class="block mb-2 text-sm font-bold text-gray-700" for="password">
-									Password
-								</label>
-								<input
-									class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-									id="password"
-									type="password"
-									placeholder="******************"
-								/>
-								<p class="text-xs italic text-red-500">Please choose a password.</p>
-							</div>
-							<div class="mb-4">
-								<input class="mr-2 leading-tight" type="checkbox" id="checkbox_id" />
-								<label class="text-sm" for="checkbox_id">
-									Remember Me
-								</label>
-							</div>
-							<div class="mb-6 text-center">
-								<button
-									class="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-									type="button"
-								>
-									Sign In
-								</button>
-							</div>
-							<hr class="mb-6 border-t" />
-							<div class="text-center">
-								<a
-									class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
-									href="./register.html"
-								>
-									Create an Account!
-								</a>
-							</div>
-							<div class="text-center">
-								<a
-									class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
-									href="./forgot-password.html"
-								>
-									Forgot Password?
-								</a>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</body>
+<form class="login-form" action="javascript:void(0);">
+    <h1>Register</h1>
+    <div class="form-field">
+        <i class="fas fa-user"></i>
+        <input type="text" name="username" id="username" class="form-field" pattern="^[a-zA-Z0-9_-]{1,16}$" placeholder=" " required>
+        <label for="username">Username</label>
+    </div>
+    <div class="form-field">
+        <i class="fas fa-user"></i>
+        <input type="text" name="username" id="username" class="form-field" pattern="^[a-zA-Z0-9_-]{1,16}$" placeholder=" " required>
+        <label for="username">Social Club</label>
+    </div>
+    <div class="form-field">
+        <i class="fas fa-user"></i>
+        <input type="text" name="username" id="username" class="form-field" pattern="^[a-zA-Z0-9_-]{1,16}$" placeholder=" " required>
+        <label for="username">mail</label>
+    </div>
+    <div class="form-field">
+        <i class="fas fa-lock"></i>
+        <input type="password" name="password" id="password" class="form-field" placeholder=" " required>
+        <label for="password">Password</label>
+    </div>
+  <button type="submit" value="Login" class="btn">Register</button>
+</form>
+<form class="login-form" action="javascript:void(0);">
+<h1>Login</h1>
+<div class="form-field">
+        <i class="fas fa-user"></i>
+        <input type="text" name="username" id="username" class="form-field" pattern="^[a-zA-Z0-9_-]{1,16}$" placeholder=" " required>
+        <label for="username">Username</label>
+    </div>
+<div class="form-field">
+        <i class="fas fa-lock"></i>
+        <input type="password" name="password" id="password" class="form-field" placeholder=" " required>
+        <label for="password">Password</label>
+    </div>
+    <button type="submit" value="Login" class="btn">Connexion</button>
+</form>
 </body>
 </html>
